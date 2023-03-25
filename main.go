a85f89e33f24dbb37f104d1d1a02fa4a27003188
Yukai Huang <yukaihuangtw@gmail.com>
2023-03-25 08:45:13 +0800

    feat: fancier styling

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"strings"
 	"time"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	"github.com/muesli/reflow/wordwrap"
+
 	bubbletea "github.com/charmbracelet/bubbletea"
 	lipgloss "github.com/charmbracelet/lipgloss"
 	"github.com/chatgp/gpt3"
@@ -19,10 +23,6 @@ type ChatMessage struct {
 	content string
 }
 
-type (
-	errMsg error
-)
-
 type model struct {
 	tabIndex int
 	apiKey   string
@@ -30,7 +30,9 @@ type model struct {
 	// new chatlog should be an array of key/value pairs
 	chatLog  []ChatMessage
 	textarea textarea.Model
-	err      errMsg
+
+	viewport viewport.Model
+	ready    bool
 }
 
 func main() {
@@ -51,12 +53,10 @@ func main() {
 func initialModel() model {
 	apiKey := os.Getenv("OPENAI_API_KEY")
 
-	var message = "Welcome to the chat!"
-
 	chatLog := []ChatMessage{
 		{
 			role:    "AI",
-			content: message,
+			content: "Welcome to the chat!",
 		},
 	}
 
@@ -65,6 +65,7 @@ func initialModel() model {
 	ta.Focus()
 	ta.CharLimit = 500
 	ta.ShowLineNumbers = false
+	ta.Prompt = "┃ "
 
 	return model{
 		tabIndex: 0,
@@ -79,7 +80,13 @@ func (m model) Init() bubbletea.Cmd {
 }
 
 func (m model) Update(msg bubbletea.Msg) (bubbletea.Model, bubbletea.Cmd) {
-	var taCmd bubbletea.Cmd
+	var (
+		taCmd bubbletea.Cmd
+		vpCmd bubbletea.Cmd
+	)
+
+	m.viewport, vpCmd = m.viewport.Update(msg)
+	m.textarea, taCmd = m.textarea.Update(msg)
 
 	switch msg := msg.(type) {
 	case bubbletea.KeyMsg:
@@ -89,80 +96,120 @@ func (m model) Update(msg bubbletea.Msg) (bubbletea.Model, bubbletea.Cmd) {
 			return m, bubbletea.Quit
 
 		case bubbletea.KeyEnter:
-			var inputMessage = m.textarea.Value()
+			inputMessage := m.textarea.Value()
+
+			// trim whitespace
+			inputMessage = strings.TrimSpace(inputMessage)
+
 			if len(inputMessage) > 0 {
+				const aiText = "Hello world"
+
 				m.chatLog = append(m.chatLog, ChatMessage{
 					role:    "user",
 					content: inputMessage,
+				}, ChatMessage{
+					role:    "AI",
+					content: aiText,
 				})
-				m.textarea.SetValue("")
-				return m, m.sendMessage()
+
+				m.textarea.Reset()
+
+				m.viewport.SetContent(m.RenderChatLog())
+				m.viewport.GotoBottom()
 			}
 		}
-	case errMsg:
-		m.err = msg
-		return m, nil
 
-	case ChatMessage:
-		m.chatLog = append(m.chatLog, msg)
-		return m, nil
-	}
+	case bubbletea.WindowSizeMsg:
+		verticalMarginHeight := m.textarea.Height() + 2
 
-	m.textarea, taCmd = m.textarea.Update(msg)
-
-	return m, taCmd
-}
-
-func (m model) sendMessage() bubbletea.Cmd {
-	return func() bubbletea.Msg {
-		// Set up the GPT-3 client
-		cli, _ := gpt3.NewClient(&gpt3.Options{
-			ApiKey:  m.apiKey,
-			Timeout: 30 * time.Second,
-		})
-
-		// Request a chat completion
-		uri := "/v1/chat/completions"
-		params := map[string]interface{}{
-			"model": "gpt-3.5-turbo",
-			"messages": []map[string]interface{}{
-				{"role": "user", "content": m.textarea.Value()},
-			},
-		}
-
-		res, err := cli.Post(uri, params)
-		if err != nil {
-			log.Fatalf("request api failed: %v", err)
-		}
-
-		message := res.GetString("choices.0.message.content")
-
-		return ChatMessage{
-			role:    "AI",
-			content: message,
+		if !m.ready {
+			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+			m.viewport.SetContent(m.RenderChatLog())
+			m.ready = true
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height - verticalMarginHeight
 		}
 	}
+
+	return m, bubbletea.Batch(taCmd, vpCmd)
 }
+
+func (m model) CallChatGPTApi() string {
+	cli, _ := gpt3.NewClient(&gpt3.Options{
+		ApiKey:  m.apiKey,
+		Timeout: 30 * time.Second,
+	})
+
+	messages := []map[string]interface{}{
+		{"role": "user", "content": "Hello world"},
+	}
+
+	// Request a chat completion
+	uri := "/v1/chat/completions"
+	params := map[string]interface{}{
+		"model":    "gpt-3.5-turbo",
+		"messages": messages,
+	}
+
+	res, err := cli.Post(uri, params)
+	if err != nil {
+		log.Fatalf("request api failed: %v", err)
+	}
+
+	message := res.GetString("choices.0.message.content")
+
+	return message
+}
+
+// func (m model) sendMessage() bubbletea.Cmd {
+// 	return func() bubbletea.Msg {
+// 		// Set up the GPT-3 client
+//
+// 		return ChatMessage{
+// 			role:    "AI",
+// 			content: message,
+// 		}
+// 	}
+// }
 
 var tabStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("240")).Align(lipgloss.Bottom).Render
 var chatTextStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("15")).Align(lipgloss.Bottom).Render
 
-func (m model) View() string {
-	view := strings.Builder{}
+func (m model) RenderChatLog() string {
+	chatLogString := ""
 
-	var chatLog = ""
 	for _, message := range m.chatLog {
-		chatLog += message.role + ": " + message.content + "\n"
+		chatLogString += message.role + ": " + message.content + "\n"
 	}
 
-	view.WriteString(chatLog)
+	return chatLogString
+}
 
-	view.WriteString("\n")
-	view.WriteString(m.textarea.View())
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%d\n%s\n\n%s\n%s",
+		len(m.chatLog),
+		m.viewport.View(),
+		m.textarea.View(),
+		wordwrap.String(tabStyle("Press TAB to switch between input and buttons. Press ENTER to send a message. Press ESC or CTRL+C to exit."), m.viewport.Width),
+	)
 
-	// Render help text
-	helpText := "Press TAB to switch between input and buttons. Press ENTER to send a message. Press ESC or CTRL+C to exit."
-	view.WriteString("\n\n" + tabStyle(helpText))
-
-	return view.String()
+	// view := strings.Builder{}
+	//
+	// var chatLog = ""
+	// for _, message := range m.chatLog {
+	// 	chatLog += message.role + ": " + message.content + "\n"
+	// }
+	//
+	// view.WriteString(chatLog)
+	//
+	// view.WriteString("\n")
+	// view.WriteString(m.textarea.View())
+	//
+	// // Render help text
+	// helpText := "Press TAB to switch between input and buttons. Press ENTER to send a message. Press ESC or CTRL+C to exit."
+	// view.WriteString("\n\n" + tabStyle(helpText))
+	//
+	// return view.String()
 }
